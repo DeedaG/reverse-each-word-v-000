65cc7e7790c8ef3c85346fdea970bc761ec28ebb
Christia T <christiatidwell@bellsouth.net>
2018-04-23 02:13:59 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@
 
 def reverse_each_word(sentence1 = "Hello there, and how are you?")
   sentence1.split.each do |word|
-   return "Hello there, and how are you?".reverse
+   return word.reverse
   end
 end
