d8eed2b2c482ba3d9d70bb21d4b91c1af7957526
Christia T <christiatidwell@bellsouth.net>
2018-04-23 01:28:26 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@
 
 def reverse_each_word(sentence)
   sentence = [sentence]
-  sentence.each do |index|
-    print index[5], index[4]
+  sentence.each do |sentence|
+    puts "i am trying"
   end
 end
