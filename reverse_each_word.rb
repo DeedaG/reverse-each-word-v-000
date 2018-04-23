593a6294720ece52c788a4a979afdbec12f05712
Christia T <christiatidwell@bellsouth.net>
2018-04-23 02:05:44 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 
 
 def reverse_each_word(sentence1 = "Hello there, and how are you?")
-  sentence1.split.each do |words|
+  sentence1.split
+  sentence1.each do |words|
     return words.reverse
   end
 end
